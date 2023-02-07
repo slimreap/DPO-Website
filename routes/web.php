66fffe9bee420711368ab8c969affd246258a375
slimreap <xt202001871@wmsu.edu.ph>
2023-02-07 10:52:07 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Route::get('home/', function () {
     return view('home');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
